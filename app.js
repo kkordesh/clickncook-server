588bdbf109008ffe91bda0ffa8ae4b0bb116b9a5
Lindsey Foster <lindseyfoster922@gmail.com>
2022-02-09 16:14:29 -0500

    added login

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ const app = Express();
 const dbConnection = require("./db");
 const controllers = require("./controllers");
 
+
 app.use(Express.json());
 
 //const middleware = require("./middleware/validate-jwt");
 app.use(require("./middleware/headers"));
 //app.use(middleware.CORS);
+//app.use("/user", controllers.usercontroller);
 
 
 dbConnection.authenticate()
 .then(() => dbConnection.sync())
 .then(() => {
-    app.listen(4000, () => {
-        console.log(`[Server]: App is listening on 4000.`);
+    app.listen(process.env.PORT, () => {
+        console.log(`[Server]: App is listening on ${process.env.PORT}.`);
     });
 })
 .catch((err) => {
